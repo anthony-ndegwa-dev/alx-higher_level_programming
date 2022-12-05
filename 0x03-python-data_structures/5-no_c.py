1e797ac5d29cacdb239b640b09aa4df03c22be27
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-05 04:50:59 -0800

    Create 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    copy_string = my_string[:]
+    j = 0
+
+    for i in range(len(my_string)):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            copy_string = copy_string[:(i - j)] + my_string[(i + 1):]
+
+    return (copy_string)
