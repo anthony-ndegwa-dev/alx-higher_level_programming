704ca19497131983403790aae04150bdf5d62d9b
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-05 04:54:17 -0800

    Modify 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,5 +7,6 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if (my_string[i] == 'c' or my_string[i] == 'C'):
             copy_string = copy_string[:(i - j)] + my_string[(i + 1):]
+            j += 1
 
     return (copy_string)
