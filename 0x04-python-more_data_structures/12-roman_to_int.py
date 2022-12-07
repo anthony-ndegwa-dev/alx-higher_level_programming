e536a00851e89845d3a78551a0b4dcc4bfe8a443
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-07 02:28:20 -0800

    Edit 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
             roman_string is None):
         return (0)
 
-    rom_dic = {
+    roman_dic = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -18,12 +18,12 @@ def roman_to_int(roman_string):
     num = 0
 
     for i in range(len(roman_string)):
-        if rom_dic.get(roman_string[i], 0) == 0:
+        if roman_dic.get(roman_string[i], 0) == 0:
             return (0)
 
         if (i != (len(roman_string) - 1) and
-                rom_dic[roman_string[i]] < rom_dic[roman_string[i + 1]]):
-            num += rom_dic[roman_string[i]] * -1
+                roman_dic[roman_string[i]] < roman_dic[roman_string[i + 1]]):
+            num += roman_dic[roman_string[i]] * -1
         else:
-            num += rom_dic[roman_string[i]]
+            num += roman_dic[roman_string[i]]
     return (num)
