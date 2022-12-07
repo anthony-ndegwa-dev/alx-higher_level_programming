0ed6f9d5a4cf2b0a4d972e2407cad1a041e2ceb0
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-07 00:18:36 -0800

    Create 9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dic = a_dictionary.copy()
+    keys_list = list(new_dic.keys())
+
+    for i in keys_list:
+        new_dic[i] *= 2
+
+    return (new_dic)
