af8cea60990371dfea808cf8cdebf5f6b783ed76
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-20 08:09:34 -0800

    Edit 5-main.py code

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Define a class Square."""
+
 class Square:
     """ Initialize new square."""
     def __init__(self, size):
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
