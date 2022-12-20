bef7322af479af9fb90694c0396f359f9ca1a897
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-20 09:36:57 -0800

    Fix Bare except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     for n in range(x):
         try:
             print("{}".format(my_list[n]), end='')
-        except:
+        except Exception:
             break
         else:
             count += 1
