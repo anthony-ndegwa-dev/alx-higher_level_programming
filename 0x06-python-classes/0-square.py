96ba0eacec9b0e46ad52ed1b53dd2e3a7d9a6dba
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-20 06:38:05 -0800

    Edit docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a square."""
+"""Define a class Square."""
 class Square:
-    """Define a square."""
-
+    """Represent a square."""
     pass
