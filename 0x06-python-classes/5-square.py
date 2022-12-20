36b98820b555c99cf63322bb08211125c3c4230f
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-20 07:53:42 -0800

    Edit 5-square code

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 class Square:
-    """ Initialize new square """
+    """ Initialize new square."""
     def __init__(self, size):
         self.size = size
 
     @property
+    def size(self):
+        """Get size of square."""
+        return (self.__size)
+
+    @size.setter
     def size(self, value):
+        """Set the size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
