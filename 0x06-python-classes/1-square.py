f0c53bb9703211987e5cb0c98cd27d84109cfde9
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-12-20 06:58:17 -0800

    Edit docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
+"""Class Square that defines a square object"""
 class Square:
-    """Class Square that defines a square object"""
+    """Initialize and store the size of the square"""
     def __init__(self, size):
-        """Initialize method that stores the size of the square
-        Args - param1 (int): size of the square
-        """
         self.__size = size
