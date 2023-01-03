c3b864f7184c16d84a3bbe857b8a7d3bd2d28573
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-03 05:22:22 -0800

    Modify a function that prints Python strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include <Python.h>
 
 /**
@@ -9,26 +7,25 @@
  * Return: no return
  */
 
-void print_python_string(PyObject *p);
+void print_python_string(PyObject *p)
 {
-	PyObject *str, *repr;
+	long int length;
 
-	(void)repr;
+	fflush(stdout);
+
 	printf("[.] string object info\n");
-
-	if (strcmp(p->ob_type->tp_name, "str"))
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
+	length = ((PyASCIIObject *)(p))->length;
+
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
