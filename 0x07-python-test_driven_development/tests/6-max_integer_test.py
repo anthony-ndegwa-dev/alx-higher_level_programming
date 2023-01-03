e863fa1f0fb7041460ef3d4d75ae1e42ed30194a
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-02 23:12:07 -0800

    Add more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Unittests for max_integer([..])."""
 
 import unittest
@@ -6,17 +7,40 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Define unittests for max_integer([..])."""
+    
+    """ Tests for max_integer function"""
 
-    def test_ordered_list(self):
-        """Test ordered list of integers."""
-        ordered = [1, 2, 3, 4]
-        self.assertEqual(max_integer(ordered), 4)
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_unordered_list(self):
-        """Test unordered list of integers."""
-        unordered = [1, 2, 4, 3]
-        self.assertEqual(max_integer(unordered), 4)
+    def test_repeated_number(self):
+        self.assertEqual(max_integer([70, 70, 70]), 70)
 
-    def test_max_at_begginning(self):
-        """Test a list with a beginning max value."""
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([2.73, 6.33, -9.123, 17.4, 6.3]), 17.4)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([9, 8, 7, 6, 5]), 9)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_zero_number(self):
+         self.assertEqual(max_integer([0, 0, 0, 0]), 0)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-70, -45, -12, -3]), -3)
+
+    def test_one_number_list(self):
+        self.assertEqual(max_integer([6]), 6)
+
+    def test_string_in_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([6, '9'])
+
+    def test_number(self):
+        with self.assertRaises(TypeError):
+            max_integer(5)
+
+if __name__ == "__main__":
+    unittest.main()
