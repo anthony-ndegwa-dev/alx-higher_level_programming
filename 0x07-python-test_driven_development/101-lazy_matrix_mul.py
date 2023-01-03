5f21a3d696213966658c838233d93d8e1375123f
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-03 00:03:56 -0800

    Wrire Function that multiplies 2 matrices

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module that multiplies 2 matrices"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function that multiplies 2 matrices"""
+
+    return (np.matmul(m_a, m_b))
