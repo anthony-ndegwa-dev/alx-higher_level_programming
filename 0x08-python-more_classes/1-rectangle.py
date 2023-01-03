7ce3647e7ba3510ebff8f163ddf21aef59d8c62b
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-03 08:39:26 -0800

    Fix indentation error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,38 +13,38 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
+    @property
+    def width(self):
 
-            """Get width of the rectangle."""
+        """Get width of the rectangle."""
 
-            return self.__width
+        return self.__width
 
-        @width.setter
-        def width(self, value):
+    @width.setter
+    def width(self, value):
 
-            """Set width of the rectangle"""
+        """Set width of the rectangle"""
 
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
+    @property
+    def height(self):
 
-            """Get height of the rectangle."""
+        """Get height of the rectangle."""
 
-            return self.__height
+        return self.__height
 
-        @height.setter
-        def height(self, value):
+    @height.setter
+    def height(self, value):
 
-            """Set height of the rectangle"""
+        """Set height of the rectangle"""
 
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
