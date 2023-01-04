36fe7f5add93a281e313a3e4ec4803b8a72e9a5c
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-04 08:19:09 -0800

    Class to restrict new instance attribute first_name

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module restricting user to create first_name instance attributes"""
+
+
+class LockedClass:
+     __slots__ = ['first_name']
+
+     def __init__(self):
+         """Initialization"""
+         pass
