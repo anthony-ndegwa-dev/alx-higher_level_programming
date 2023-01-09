601c23ef07cd10a4bdbac1e826765ce6a3c05a4b
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-09 07:22:23 -0800

    Unmerge code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
-"""Class that raises an Exception"""
-
-
-class BaseGeometry:
-    """Raise an Exception area() is not implemented"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method that validates values"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""Rectangle class inheriting BaseGeometry class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Inheritance class"""
 
 
 class Rectangle(BaseGeometry):
+    """A class that inherits BaseGeometry"""
+
     def __init__(self, width, height):
         """Initialize instances"""
 
