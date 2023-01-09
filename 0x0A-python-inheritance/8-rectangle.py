19dc3c78aecaa7b8f9dbf45f76ad1c7ff2197d58
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-09 06:47:29 -0800

    Add blank line below docstring

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 """A class Rectangle that inherits from BaseGeometry"""
 
 
 class Rectangle(BaseGeometry):
-    """Alass that inherits from BaseGeometry"""
+    """A class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
         """Initialize instances"""
