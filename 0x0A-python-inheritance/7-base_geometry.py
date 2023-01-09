968dbe25482dab5232bb6aed1318e6dc71c04dbb
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-09 06:33:46 -0800

    Instance variable

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,6 +12,6 @@ class BaseGeometry:
         """Method that validates values"""
 
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(value))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(value))
+            raise ValueError("{} must be greater than 0".format(name))
