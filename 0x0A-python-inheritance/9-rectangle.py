47753ff7c68f57af13aae5030cd4e7ada26be0e7
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-09 07:52:36 -0800

    Fix error

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -25,4 +25,6 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """Returns the printable string"""
 
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
