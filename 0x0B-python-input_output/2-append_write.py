5f45d367ae57e002d736dcc6b9cd7ab75b99d3c7
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-10 04:32:24 -0800

    Function to append text in a text file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A function that appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """Append a string in text file and return number of characters added"""
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.append(text)
