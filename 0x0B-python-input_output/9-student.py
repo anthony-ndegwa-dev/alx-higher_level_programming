853dd480ea68ada7f236f1ed14ca0d4287a33964
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-10 05:53:31 -0800

    A class Student that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A class Student that defines a student"""
+
+
+class Student:
+    """Define a studen"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Method to retrieve dictionary representation of Student instance"""
+        return seld.__dict__.copy()
