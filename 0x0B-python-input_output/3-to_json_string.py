1610fd30916a0b631df4d9a585e037396827adc9
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-10 04:40:33 -0800

    Function to return JSON representation of string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""A function to return JSON representation of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """Return JSON representation of string"""
+    return json.dumps(my_obj)
