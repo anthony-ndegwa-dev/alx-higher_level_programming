c8f42159e27ec78278f9d12875ca60e40bce51b6
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-10 06:51:54 -0800

    Fix error

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,4 +12,4 @@ def append_after(filename="", search_string="", new_string=""):
             if search_string in line:
                 text += new_string
     with open(filename, 'w', encoding="utf-8") as f:
-        w.write(text)
+        f.write(text)
