2382b7ccd7e5c286d011e4762668dfb5001719e2
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-14 07:05:08 -0800

    Add class constructor and initialize instances

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module containing class Rectangle that inherits from Base """
+from base import Base
+
+
+clase Rectangle(Base):
+    """The Rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize instances"""
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
