2bc8eff40691ebd7ee9533984c309be65b6f9e2f
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-14 05:50:43 -0800

    Write the first base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module with the class Base """
+
+
+class Base:
+    """ Class constructor """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
