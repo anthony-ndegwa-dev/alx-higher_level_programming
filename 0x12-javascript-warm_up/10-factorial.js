2bf7003b0b5bb18b434a7a446fdf351bc22a6b1d
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-13 08:45:51 -0700

    Script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function factorial (num) {
+  if ((Number.isNaN(num)) || (num === 1)) {
+    return 1;
+  }
+  return factorial(num - 1) * num;
+}
+console.log(factorial(parseInt(process.argv[2])));
