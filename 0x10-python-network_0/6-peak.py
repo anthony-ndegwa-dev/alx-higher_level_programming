9dc4ef8298c14bbda2b9064f5615f3a6e6a24cae
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-30 01:35:24 -0700

    A function that finds a peak in a list of unsorted integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Python script to find a peak inside a list """
+
+
+def find_peak(list_of_integers):
+    if list_of_integers == []:
+        return None
+
+    length = len(list_of_integers)
+    center = int(length / 2)
+    li = list_of_integers
+
+    if center - 1 < 0 and center + 1 >= length:
+        return li[center]
+    elif center - 1 < 0:
+        return li[center] if li[center] > li[center + 1] else li[center + 1]
+    elif center + 1 >= length:
+        return li[center] if li[center] > li[center - 1] else li[center - 1]
+
+    if li[center - 1] < li[center] > li[center + 1]:
+        return li[center]
+
+    if li[center + 1] > li[center - 1]:
+        return find_peak(li[center:])
+    return find_peak(li[:center])
