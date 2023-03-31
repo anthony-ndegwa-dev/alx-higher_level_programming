41fc4245c7d8cb2a180107fef2890f237ea05ac0
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-31 08:18:31 -0700

    Use shorter variables to meet pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-        as response:
-        page = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as alx:
+        page = alx.read()
         print('Body response:')
         print('\t- type: {}'.format(type(page)))
         print('\t- content: {}'.format(page))
