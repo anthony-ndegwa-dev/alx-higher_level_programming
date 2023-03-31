2e5b4d08b4833553c8022de64916a993a3a77a77
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-30 22:46:18 -0700

    Update the curl command to get the right output

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond You got me!.
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin: You got me!"
+curl -s -L  -X PUT  -H 'Origin: School' localhost:5000/catch_me_3
