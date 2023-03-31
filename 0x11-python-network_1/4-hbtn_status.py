0b8527d9037ab8e622bf335a79768cf2e39b0f16
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-31 09:23:22 -0700

    Fix variable miss match

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,5 +12,5 @@ import request
 
 if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
-    b = read.text
+    b = r.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(b), b))
