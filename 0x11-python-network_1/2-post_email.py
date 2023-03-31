0388e860045396e5e4fbf2b122267fd0d4d0efa8
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-31 08:40:24 -0700

    Script takes in URL & email, sends POST request to passed URL with email as a parametere

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8)
+
+    The email must be sent in the email variable
+    You must use the packages urllib and sys
+    You are not allowed to import packages other than urllib and sys
+    You don’t need to check arguments passed to the script (number or type)
+    You must use the with statement
+"""
+import sys
+from urllib import request, parse
+
+
+if __name__ == '__main__':
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        page = response.read()
+        print(page.decode('utf-8'))
