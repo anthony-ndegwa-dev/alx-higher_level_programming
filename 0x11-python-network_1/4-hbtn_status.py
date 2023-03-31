b397116d28129ef3a7f4e37b3903a07df079329e
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-31 09:55:22 -0700

    Fix link spelling error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,6 +11,6 @@ import requests
 
 
 if __name__ == '__main__':
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     b = r.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(b), b))
