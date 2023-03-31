3dd4d4ad43f8907ecb8277b1fe5621f9df3dddad
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-03-31 09:18:18 -0700

    Script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+
+    You must use the package requests
+    You are not allow to import packages other than requests
+    The body of the response must be display like the following example
+    (tabulation before -)
+"""
+import request
+
+
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    b = read.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(b), b))
