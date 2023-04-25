7f528efeed532b0bb9c52a845e438cedc695dc33
root <anthony.n.ndegwa@gmail.com>
2023-04-25 01:25:33 -0700

    Script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+/**
+ * A script that prints all characters of a Star Wars movie:
+ * The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+ * Display one character name by line in the same order of the list
+ *  “characters” in the /films/ response
+ * You must use the Star wars API
+ * You must use the module request
+ */
+const request = require('request');
+const url = 'http://swapi.co/api/films/';
+let id = parseInt(process.argv[2], 10);
+let characters = [];
+
+request(url, function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    if (id < 4) {
+      id += 3;
+    } else {
+      id -= 3;
+    }
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].episode_id === id) {
+        characters = results[i].characters;
+        break;
+      }
+    }
+    for (let j = 0; j < characters.length; j++) {
+      request(characters[j], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
